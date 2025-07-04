454da1a691978372f5699fca2056b37b783ee994
Vishal <151914635+vishalcode2023@users.noreply.github.com>
2025-07-04 18:46:59 +0530

    Update VideoSection.jsx

--- app/Components/LandingPages/VideoSection.jsx
+++ app/Components/LandingPages/VideoSection.jsx
@@ -2,63 +2,65 @@ import React from "react";
 
 const VideoSection = () => {
   return (
-    <div className="px-6 py-12 ">
+    <div className="px-6 py-12">
       <div className="text-center mb-12">
-        <h2 className="text-3xl font-bold text-gray-800">Explore Our Video </h2>
+        <h2 className="text-3xl font-bold text-gray-800">Explore Our Videos</h2>
         <p className="text-lg text-gray-600">
-          Check out these insightful videos to learn more about our services and
-          products.
+          Check out these insightful videos to learn more about our services and products.
         </p>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-        {/* Video 1 */}
+        {/* Cloudinary Video 1 */}
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="p-4">
             <iframe
               width="100%"
               height="400px"
-              src="https://www.youtube.com/embed/TclxgUBZiN0"
-              title="Video 1"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allow="autoplay; fullscreen; encrypted-media"
               allowFullScreen
+              src="https://player.cloudinary.com/embed/?cloud_name=dt2juqy9s&public_id=5822ac68-f2a8-403a-af05-8402202d9002_vc5u17&profile=cld-default"
+              title="Cloudinary Video 1"
+              frameBorder="0"
             ></iframe>
           </div>
         </div>
 
-        {/* Video 2 */}
+        {/* Cloudinary Video 2 */}
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="p-4">
             <iframe
               width="100%"
               height="400px"
-              src="https://www.youtube.com/embed/LHFwkxbFzig"
-              title="Video 2"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allow="autoplay; fullscreen; encrypted-media"
               allowFullScreen
+              src="https://player.cloudinary.com/embed/?cloud_name=dt2juqy9s&public_id=cccf6aa4-8c6f-467c-a1f0-e08629e44969_wvcatj&profile=cld-default"
+              title="Cloudinary Video 2"
+              frameBorder="0"
             ></iframe>
           </div>
         </div>
 
-        {/* Video 3 */}
         <div className="bg-white rounded-lg shadow-lg overflow-hidden">
           <div className="p-4">
             <iframe
               width="100%"
               height="400px"
-              src="https://www.youtube.com/embed/hWz0s3iwPXg"
-              title="Video"
-              frameBorder="0"
-              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allow="autoplay; fullscreen; encrypted-media"
               allowFullScreen
+              src="https://player.cloudinary.com/embed/?cloud_name=dt2juqy9s&public_id=e29c5a71-c641-4592-a878-d7b633589415_g5nbi7&profile=cld-default"
+              title="Cloudinary Video 2"
+              frameBorder="0"
             ></iframe>
           </div>
         </div>
+
+
+        
       </div>
     </div>
   );
 };
 
 export default VideoSection;
+
